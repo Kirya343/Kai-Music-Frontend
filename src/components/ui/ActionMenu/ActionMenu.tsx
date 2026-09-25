dc40e64286e47c4a9e292f79be5ebb968b2af4d5
kirya343 <kkodolov40@gmail.com>
2026-09-25 17:32:36 +0300

    icons and actionMenu

---
+++ src/components/ui/ActionMenu/ActionMenu.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import styles from "./ActionMenu.module.scss";
+import EllipsisVerticalIcon from "@/components/icons/EllipsisVerticalIcon";
+import clsx from "clsx";
+
+export interface IKebabAction {
+    title: string,
+    func: () => void,
+    icon?: string,
+    access?: boolean
+};
+
+interface ActionMenuProps {
+    actions: IKebabAction[];
+    className?: string;
+}
+
+const ActionMenu = ({ actions, className }: ActionMenuProps) => {
+    const filtered = actions.filter((action) => action.access ?? true);
+
+    if (filtered.length === 0) {
+        return null;
+    }
+
+    return (
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+                <button
+                    type="button"
+                    className={clsx(styles.button, className, "hover")}
+                    aria-label="Действия"
+                >
+                    <EllipsisVerticalIcon className={styles.icon} />
+                </button>
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+                <DropdownMenu.Content
+                    className={styles.list}
+                    sideOffset={4}
+                    align="start"
+                >
+                    {filtered.map((action) => (
+                        <DropdownMenu.Item
+                            key={action.title}
+                            className={`${styles.item} hover`}
+                            onSelect={action.func}
+                        >
+                            {action.icon && (
+                                <div className={styles.itemIcon}>
+                                    <i
+                                        className={`fa-regular fa-${action.icon} fa-lg`}
+                                    />
+                                </div>
+                            )}
+
+                            <span>{action.title}</span>
+                        </DropdownMenu.Item>
+                    ))}
+                </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+        </DropdownMenu.Root>
+    );
+};
+
+export default ActionMenu;
